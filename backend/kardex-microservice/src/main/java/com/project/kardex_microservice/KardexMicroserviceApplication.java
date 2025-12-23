0700b65ee7c0a62527397558853a5f3303e8f9cc
locomotive1584 <benjamin.araya@usach.cl>
2025-12-23 01:42:35 -0300

    Creacion de microservicios

---
+++ backend/kardex-microservice/src/main/java/com/project/kardex_microservice/KardexMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.project.kardex_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KardexMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KardexMicroserviceApplication.class, args);
+	}
+
+}
